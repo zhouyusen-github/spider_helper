135ef749492bd1a80389a853cbd34a123cd83634
Windcolour <1015357985@qq.com>
2020-02-09 19:36:23 +0800

    markdown_to_python:解决了子字段问题

--- markdown_to_python.py
+++ markdown_to_python.py
@@ -1,24 +1,15 @@
-# 将markdown里面的描述数据库的表格代码转换为sql建表语句
+"""
+将markdown里面的描述数据库的表格代码转换为python解析接口代码
+"""
 
 
-# 我觉得用python readline好
-
-# 字段名，数据库类型，注释都要自动处理
-'''
-    def save_string_safely(field):
-        if type(field) != str:
-            return str(field)
-        else:
-            return field
-'''
-
-f = open('markdown_example', encoding='utf-8')
-
-jsonname = "POI"  # 要解析的json，你在代码中定义的名字
-for line in f.readlines():
+# 清理好，并切割好每行markdown代码中的各变量
+def clean_split_line(line):
     line_clean = line.replace(" ", "")
     line_clean = line_clean.replace("\n", "")
-    line_clean = line_clean.replace("\|", "@$@")  # 用一些字段名不用用到的字符先暂时替换所有字符\|
+    line_clean = line_clean.replace("\r", "")
+
+    line_clean = line_clean.replace("\\|", "@$@")  # 用一些字段名不用用到的字符先暂时替换所有字符\|,\|是为了防止|被识别
     strings = line_clean.split("|")
 
     # 把@$@替换回来\|
@@ -27,28 +18,33 @@ for line in f.readlines():
         string = string.replace("@$@", "|")
         strings_append.append(string)
     strings = strings_append
+    return strings
 
-    # print(strings)
 
-    # print("`{field}` {char} DEFAULT NULL  # {note}".format(field=strings[0], char=strings[3], note=strings[2]))
+f = open('markdown_example', encoding='utf-8')
+json_name = "POI"  # 要解析的json在你的代码中定义的名字
+separation_character = '-'  # 用于分割字段与子字段的字符
+
+for line in f.readlines():
+    strings = clean_split_line(line)
     database_field_name = strings[0]
     json_field_name = strings[1]
     meaning = strings[2]
     database_field_type = strings[3]
     preprocessing = strings[5]
     allow_null = strings[7]
-    if "varchar" in database_field_type:
-        database_field_type = 'str'
 
-    field_value_split = json_field_name.split('-')
-    if len(field_value_split) == 2:
-        answer = "'{field}': self.safe_str({jsonname}.get('{field_value1}', safe_change).get('{field_value2}', None)),".format(field=database_field_name, jsonname=jsonname, field_value1=field_value_split[0], field_value2=field_value_split[1])
-        answer = answer.replace('safe_change','{}')
-        print(answer)
-
+    if database_field_name == 'snapshot_time':
+        print("'snapshot_time': datetime.utcnow()")
     else:
-        if database_field_name != 'snapshot_time':
-            print("'{field}': self.safe_str({jsonname}.get('{field_value}', None)),".format(field=database_field_name, jsonname=jsonname,
-                                                                                            field_value=json_field_name))
-        else:
-            print("'snapshot_time': datetime.utcnow()")
+        json_field_name_split = json_field_name.split(separation_character)
+        answer = "'{database_field_name}': self.{preprocessing}({json_name}".format(
+            database_field_name=database_field_name,
+            preprocessing=preprocessing.strip(')'),
+            json_name=json_name
+        )
+        for i in range(len(json_field_name_split) - 1):
+            string = ".get('{field_value}', {empty_dict})".format(field_value=json_field_name_split[i], empty_dict='{}')
+            answer = answer + string
+        answer = answer + ".get('{field_value}', None))".format(field_value=json_field_name_split[-1])
+        print(answer)
