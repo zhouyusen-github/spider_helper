8f4ad939e258e31e7777c39e4330150954b98504
Windcolour <1015357985@qq.com>
2020-02-09 14:27:15 +0800

    json_to_markdown.py:添加实现markdown的'数据库字段类型'，'预处理'的功能，添加解析子字段功能，添加格式如表格开头代码，添加结尾snapshot_time

--- json_to_markdown.py
+++ json_to_markdown.py
@@ -1,54 +1,85 @@
 # 接口数据直接转化为markdown数据
+# varchar长度后期要改，因为数据多了，奇怪的情况就会出现
 
-'''
-{
-            "id": "B0FFH1UBMJ",
-            "parent": [],
-            "childtype": [],
-            "name": "中惠泽石油全福加油加气站",
-            "type": "汽车服务;其它能源站;其它能源站",
-            "typecode": "010200",
-            "biz_type": [],
-            "address": "二环东路200号",
-            "location": "117.064161,36.704177",
-            "tel": "0531-88967066",
-            "distance": [],
-            "biz_ext": {
-                "rating": "4.5",
-                "cost": []
-            },
-            "pname": "山东省",
-            "cityname": "济南市",
-            "adname": "历城区",
-            "importance": [],
-            "shopid": [],
-            "shopinfo": "0",
-            "poiweight": [],
-            "photos": [
-                {
-                    "url": "http://store.is.autonavi.com/showpic/221f5783ffa129e7179f97aff66b67bf",
-                    "provider": [],
-                    "title": []
-                },
-                {
-                    "url": "http://store.is.autonavi.com/showpic/9a11fe061a2c6feb20da0ac84c99781e",
-                    "provider": [],
-                    "title": []
-                },
-                {
-                    "url": "http://store.is.autonavi.com/showpic/8428bae578c26fb75408272a2996d8d7",
-                    "provider": [],
-                    "title": []
-                }
-            ]
-        }
-'''
 import json
-f = open('json_example')
+import datetime
+
+
+# 分析字段内容类型，填入markdown中'数据库字段类型'，'预处理'位置
+def type_indentify(value):
+    answer = {'type': '', 'preprocessing': ''}
+    value_type = type(value)
+    if value_type != str:
+        if value_type == int:
+            answer['type'] = 'int'
+        elif value_type == float:
+            answer['type'] = 'float'
+        elif value_type == list:
+            if len(value) == 0:  # 应付[]这种异常
+                answer['type'] = 'varchar(40)'
+            else:
+                answer['type'] = 'varchar({length})'.format(length=str(len(str(value))))
+            answer['preprocessing'] = 'safe_str()'
+        elif value_type == dict:
+            answer['type'] = 'dict'  # 有子字段另外处理
+    elif value_type == str:
+        try:
+            int(value)
+            if int(value[0]) != 0:  # 排除int('010200')
+                answer['type'] = 'int'
+                answer['preprocessing'] = 'safe_int()'
+            else:
+                answer['type'] = 'varchar({length})'.format(length=str(len(str(value))))
+                answer['preprocessing'] = 'safe_str()'
+        except:
+            try:  # 先int再float，因为float可以转化int的字符串的，反之不行
+                float(value)
+                answer['type'] = 'float'
+                answer['preprocessing'] = 'safe_float()'
+            except:
+                try:
+                    datetime.datetime.strptime(value, "%Y-%m-%d %H:%M:%S")
+                    answer['type'] = 'datetime'
+                    answer['preprocessing'] = 'safe_datetime()'
+                except:
+                    try:
+                        datetime.datetime.strptime(value, "%Y-%m-%d")
+                        answer['type'] = 'date'
+                        answer['preprocessing'] = 'safe_date()'
+                    except:
+                        try:
+                            datetime.datetime.strptime(value, "%H:%M:%S")
+                            answer['type'] = 'time'
+                            answer['preprocessing'] = 'safe_time()'
+                        except:
+                            answer['type'] = 'varchar({length})'.format(length=str(len(str(value))))
+                            answer['preprocessing'] = 'safe_str()'
+    return answer
+
+
+def print_markdown(json_dict, father_key=None):  # 输出markdown表格文件
+    for key in json_dict:
+        if isinstance(json_dict[key], str):
+            value = json_dict[key].replace("|", "\|")
+        else:
+            value = json_dict[key]
+        answer = type_indentify(value)
+        value_type = answer['type']
+        if father_key:
+            key = father_key + '-' + key
+        preprocessing = answer['preprocessing']
+        print("--- | {key} | --- | {type} | {value} | {preprocessing}".format(key=key,
+                                                                              type=value_type,
+                                                                              value=value,
+                                                                              preprocessing=preprocessing))
+        if value_type == 'dict':  # 如果有子字段也要解析
+            print_markdown(value, key)
+
+
+f = open('json_example', encoding='utf-8')
 json_string = f.read()
-json = json.loads(json_string)
-print(json)
-for key in json:
-    if isinstance(json[key], str):
-        value = json[key].replace("|","\|")
-    print("--- | {key} | --- | --- | {value} | ---".format(key=key,value=value))
+json_dict = json.loads(json_string)
+print('数据库字段名 | 接口字段名 | 意义 | 数据库字段类型 | 例子 | 预处理 |异常返回| 不可为null')
+print('---|---|---|---|---|---|---|---')
+print_markdown(json_dict)
+print("snapshot_time | --- | --- | datetime | --- |")
